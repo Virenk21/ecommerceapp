5d1a81c9c3765fe27a584b00dd689909be85afd4
Virenk21 <viren.d222111@gmail.com>
2023-02-08 20:50:49 +0530

    updated brand name

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-white py-2 shadow-sm">
          <div className="container">
-            <NavLink className="navbar-brand fw-bold fs-4" to="/">LA COLLECTION</NavLink>
+            <NavLink className="navbar-brand fw-bold fs-4" to="/">Shekhar's COLLECTION</NavLink>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
