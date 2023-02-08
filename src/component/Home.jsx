433c23f8faffd53a3ef8d8efd2302024b4b1171e
Virenk21 <viren.d222111@gmail.com>
2023-02-08 14:32:38 +0530

    added title

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         <div className="card-img-overlay d-flex flex-column justify-content-center">
           <div className="container">
             <h5 className="card-title display-3 fw-bolder mb-0">
-              NEWssss SEASON ARRIVALS
+              NEWsssssssssssssss SEASON ARRIVALS
             </h5>
             <p className="card-text lead fs-2">CHECK OUT ALL THE LATEST TRENDS</p>
           </div>
